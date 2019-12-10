703da2e06dbc3a0e391dc179f2cc1caad8408ac6
Felipe <felipe.rce@gmail.com>
2019-12-10 20:44:03 -0300

    implemented service interface

---
+++ app/src/main/java/com/example/chucknorrisjokes/data/remote/response/Category.kt
@@ -0,0 +1,5 @@
+package com.example.chucknorrisjokes.data.remote.response
+
+data class Category(
+    var category: String
+)
--- app/src/main/java/com/example/chucknorrisjokes/data/remote/service/ChuckService.kt
+++ app/src/main/java/com/example/chucknorrisjokes/data/remote/service/ChuckService.kt
@@ -1,8 +1,19 @@
 package com.example.chucknorrisjokes.data.remote.service
 
+import com.example.chucknorrisjokes.data.remote.response.Category
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ChuckService {
     companion object {
         const val RANDOM_JOKE_PATH = "jokes/random"
         const val CATEGORIES_PATH = "jokes/categories"
     }
+
+    @GET(CATEGORIES_PATH)
+    fun getRandomJoke() : Deferred<Response<List<Category>>>
+
+    @GET(CATEGORIES_PATH)
+    fun getCategories() : Deferred<Response<List<Category>>>
 }
